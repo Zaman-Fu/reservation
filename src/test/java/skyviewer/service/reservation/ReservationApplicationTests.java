aafb4a8b23db7f1ce2f7b80cb5fbe5d9454cbc09
Philip Zamanis <philip.zamanis@solera.com>
2022-11-03 15:08:43 +0100

    reservation api testing

--- src/test/java/skyviewer/service/reservation/ReservationApplicationTests.java
+++ src/test/java/skyviewer/service/reservation/ReservationApplicationTests.java
@@ -1,13 +1,46 @@
 package skyviewer.service.reservation;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import skyviewer.service.reservation.controllers.ReservationController;
+import skyviewer.service.reservation.entities.Reservation;
 
 @SpringBootTest
 class ReservationApplicationTests {
 
+	@Autowired
+	ReservationController controller;
+	
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
+	
+	@Test
+	void CreateNewReservation_ValidInput_ReturnsCreated()
+	{
+		Reservation newRes=new Reservation(1000l,1l,"Dummy","Mockson","FF999999",
+				"other",false,900f);
+		List<Reservation> newResList=new ArrayList<Reservation>();
+		newResList.add(newRes);
+		ResponseEntity<List<Reservation>> response=controller.PostReservations(newResList);
+		
+		
+		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+	}
+	
+	
+	
+	
 
 }
